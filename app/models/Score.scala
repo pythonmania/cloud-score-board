32ee0375a528b92ecba555c1f68599a7872198f0
seung lee <ls@ls-home-pc.(none)>
2012-05-13 20:31:50 +0900

    added postgresql dependency, finetuned scala build by heroku buildpack.

--- app/models/Score.scala
+++ app/models/Score.scala
@@ -3,22 +3,29 @@ package models
 import redis.clients.jedis._
 import scala.collection.JavaConversions._
 import scala.collection.immutable.SortedSet
+import java.net.URI
 
 case class Score(userid: String, score: Long)
 
 object Score {
-  val pool = new JedisPool(new JedisPoolConfig(), "localhost");
+  val redisURI = new URI(System.getenv("REDISTOGO_URL"));
+  val pool = new JedisPool(new JedisPoolConfig(),
+    redisURI.getHost(),
+    redisURI.getPort(),
+    Protocol.DEFAULT_TIMEOUT,
+    redisURI.getUserInfo().split(":", 2)(1));
 
   def create(score: Score) {
     val jedis = pool.getResource()
     jedis.zadd("highscore", score.score, score.userid)
+    pool.returnResource(jedis)
   }
 
   def all(): Seq[Score] = {
-    val jedis = pool.getResource()    
+    val jedis = pool.getResource()
     val scoresTuple = jedis.zrevrangeWithScores("highscore", 0, 10).toSeq
     pool.returnResource(jedis)
-    
+
     scoresTuple map (tuple => {
       Score(tuple.getElement(), tuple.getScore().toLong)
     })
--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "redis.clients" % "jedis" % "2.0.0")
+    "redis.clients" % "jedis" % "2.0.0",
+    "postgresql" % "postgresql" % "9.1-901.jdbc4")
 
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings()
 
